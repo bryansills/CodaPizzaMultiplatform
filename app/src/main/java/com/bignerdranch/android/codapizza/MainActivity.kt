247f593e5d9bcce2a33284b6b5bd1a03603c4a8c
bryansills <bsills@bignerdranch.com>
2023-02-21 13:01:23 -0500

    Move the app over here

--- app/src/main/java/com/bignerdranch/android/codapizza/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/codapizza/MainActivity.kt
@@ -3,44 +3,16 @@ package com.bignerdranch.android.codapizza
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.bignerdranch.android.codapizza.ui.theme.CodaPizzaMultiplatformTheme
+import com.bignerdranch.android.codapizza.ui.AppTheme
+import com.bignerdranch.android.codapizza.ui.PizzaBuilderScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CodaPizzaMultiplatformTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+            AppTheme {
+                PizzaBuilderScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CodaPizzaMultiplatformTheme {
-        Greeting("Android")
     }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
 //    kotlin("multiplatform").version("1.8.0") apply false
     kotlin("android").version("1.8.0") apply false
+    kotlin("plugin.parcelize").version("1.8.0") apply false
     id("com.android.application").version("8.0.0-beta03") apply false
     id("com.android.library").version("8.0.0-beta03") apply false
 //    id("org.jetbrains.compose").version("1.3.0") apply false
